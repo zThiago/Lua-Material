fe6c8aec32f03b99d16993034e5fb32436db19a1
Thiago Cunha Ferreira <50816917+zThiago@users.noreply.github.com>
2020-08-08 01:28:14 -0300

    Create code01.lua
    Variaveis,Comentarios e print

---
+++ inicial/code01.lua
@@ -0,0 +1,10 @@
+-- O comentario da Lua diferente de outras linguagens e feita usando 2 hífen, após isso pode ser feito o comentario.
+
+-- Criação de uma variavél
+texto = "Olá Mundo"
+-- Retorna o valor no console.
+print(texto)
+
+-- Alterando o valor de uma variavél pré-definida
+texto = "Esse texto foi alterado'
+print(texto)
