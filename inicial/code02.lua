f6ff175c9a713aca72c4b92b273f143632210a2c
Thiago Cunha Ferreira <50816917+zThiago@users.noreply.github.com>
2020-08-09 16:32:57 -0300

    Condição 1

---
+++ inicial/code02.lua
@@ -0,0 +1,15 @@
+-- Condinções: IF
+-- Simulando uma situação de compra
+valor = 5
+quantia = 6
+if quantia >= valor do
+print("Comprado")
+else 
+print("Dinheiro insuficiente")
+end
+
+-- IF = Se, onde e proposto uma verificação se a quantia e maior ou igual ao o valor, se o mesmo for verdadeiro[TRUE] ele irá printar "comprado"
+-- ELSE = Caso contrario,Serve para impor uma ação caso a verificação retorne falsa[FALSE] onde a quantia não e maior ou igual ao valor, nesse caso será printado "Dinheiro insuficiente"
+-- DO = Faça, informar o que será feito na condição
+-- End = fim, termina a condição
+-- Nesse primeiro caso a condição será verdadeira, pois a quantia e maior ou igual ao valor
