0ad909c0a3e41530551faab90fb9f1a3f0242b27
Thiago Cunha Ferreira <50816917+zThiago@users.noreply.github.com>
2020-08-14 19:26:11 -0300

    Create code03.lua

---
+++ inicial/code03.lua
@@ -0,0 +1,11 @@
+print(2 > 1)
+-- True = 2 e maior que 1
+
+print(1 > 2)
+-- False = 1 não e maior que 1
+
+print(5 = 5)
+-- True = 5 e igual a 5
+
+print(5 >= 7)
+-- False = 5 não e maior ou igual a 7
